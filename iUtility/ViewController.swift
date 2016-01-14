a353b903e6919e0fefc0eda4f93c6d0ab70c274a
Kyle Yang <yangzychina@gmail.com>
2016-01-14 15:44:34 +0800

    Initial Commit

---
+++ iUtility/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iUtility
+//
+//  Created by 杨志赟 on 16/1/14.
+//  Copyright © 2016年 xiaoluuu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
